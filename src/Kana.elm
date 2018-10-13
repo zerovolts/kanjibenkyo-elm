2bf2cd1a2e7d4c960cbb13d43c5426aa9020b24b
Zach Stone <hexaphixylix@yahoo.com>
2018-10-13 14:26:22 -0400

    wip

---
+++ src/Kana.elm
@@ -0,0 +1,100 @@
+module Kana exposing (..)
+
+import Char
+
+
+type alias Kana =
+    { hiragana : Char
+    , katakana : Char
+    , romaji : String
+    }
+
+
+default : Kana
+default =
+    { hiragana = 'あ'
+    , katakana = 'ア'
+    , romaji = "a"
+    }
+
+
+type VowelCategory
+    = A
+    | I
+    | U
+    | E
+    | O
+
+
+vowelCategoryToIndex : VowelCategory -> Int
+vowelCategoryToIndex cat =
+    case cat of
+        A ->
+            0
+
+        I ->
+            1
+
+        U ->
+            2
+
+        E ->
+            3
+
+        O ->
+            4
+
+
+isKana : Char -> Bool
+isKana char =
+    isHiragana char || isKatakana char
+
+
+isHiragana : Char -> Bool
+isHiragana char =
+    let
+        code =
+            Char.toCode char
+    in
+    code >= 12352 && code <= 12447
+
+
+isKatakana : Char -> Bool
+isKatakana char =
+    let
+        code =
+            Char.toCode char
+    in
+    code >= 12448 && code <= 12543
+
+
+kanaGroups : List String
+kanaGroups =
+    [ "あいうえお"
+    , "かきくけこ"
+    , "さしすせそ"
+    , "たちつてと"
+    , "なにぬねの"
+    , "はひふへほ"
+    , "まみむめも"
+    , "らりるれろ"
+    , "がぎぐげご"
+    , "ざじずぜぞ"
+    , "だぢづでど"
+    , "ばびぶべぼ"
+    , "ぱぴぷぺぽ"
+    ]
+
+
+changeVowel : Char -> VowelCategory -> Maybe Char
+changeVowel kana cat =
+    let
+        index =
+            vowelCategoryToIndex cat
+    in
+    kanaGroups
+        |> List.filter (String.any (\c -> c == kana))
+        |> List.head
+        |> Maybe.andThen (\x -> Just (String.slice index (index + 1) x))
+        |> Maybe.andThen String.uncons
+        |> Maybe.andThen (\x -> Just (Tuple.first x))
---
+++ src/Word.elm
@@ -0,0 +1,104 @@
+module Word exposing (..)
+
+import Dict exposing (Dict)
+import Kana
+import Kanji exposing (Kanji)
+
+
+type alias Word =
+    { word : String
+    , category : WordCategory
+    , meanings : List String
+    }
+
+
+type WordCategory
+    = Verb VerbCategory
+    | Noun
+    | Adjective
+    | Adverb
+    | Particle
+
+
+type VerbCategory
+    = Ichidan
+    | Godan
+
+
+default : Word
+default =
+    { word = "見る"
+    , category = Verb Ichidan
+    , meanings = [ "to see" ]
+    }
+
+
+defaultGodan : Word
+defaultGodan =
+    { word = "行く"
+    , category = Verb Godan
+    , meanings = [ "to go" ]
+    }
+
+
+getKanji : Dict Char Kanji -> Word -> List Kanji
+getKanji allKanjiDict word =
+    word.word
+        |> String.toList
+        |> List.filter (\char -> Kanji.isKanji char)
+        |> List.foldl
+            (\char kanjiList ->
+                case Dict.get char allKanjiDict of
+                    Just kanji ->
+                        kanji :: kanjiList
+
+                    Nothing ->
+                        kanjiList
+            )
+            []
+
+
+toPast : Word -> Maybe Word
+toPast word =
+    case word.category of
+        Verb Ichidan ->
+            Just
+                { word
+                    | word =
+                        word.word
+                            |> String.slice 0 -1
+                            |> (\x -> String.append x "た")
+                }
+
+        Verb Godan ->
+            Just word
+
+        _ ->
+            Nothing
+
+
+toNegative : Word -> Maybe Word
+toNegative word =
+    case word.category of
+        Verb Ichidan ->
+            Just
+                { word
+                    | word =
+                        word.word
+                            |> String.slice 0 -1
+                            |> (\x -> String.append x "ない")
+                }
+
+        Verb Godan ->
+            let
+                ( vowel, root ) =
+                    Maybe.withDefault ( 'あ', "" ) (String.uncons (String.reverse word.word))
+            in
+            Just
+                { word
+                    | word =
+                        String.reverse root ++ String.cons (Maybe.withDefault 'あ' (Kana.changeVowel vowel Kana.A)) "ない"
+                }
+
+        _ ->
+            Nothing
