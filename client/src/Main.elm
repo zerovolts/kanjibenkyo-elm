c418f812baa68bf41d94621fddf4d72b14747ee1
Zach Stone <zach@zerovolts.com>
2018-10-13 17:32:20 -0400

    move elm files into client directory

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,9 +1,12 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
+import Browser exposing (Document)
+import Browser.Navigation exposing (Key)
 import Dict exposing (Dict)
 import Html exposing (Html, div, h1, img, text)
 import Kana exposing (Kana)
 import Kanji exposing (Kanji)
+import Url exposing (Url)
 import Word exposing (Word)
 
 
@@ -14,8 +17,8 @@ type alias Model =
     }
 
 
-init : ( Model, Cmd Msg )
-init =
+init : () -> Url -> Key -> ( Model, Cmd Msg )
+init flags url key =
     ( { kana = Dict.singleton (.hiragana Kana.default) Kana.default
       , kanji = Dict.singleton (.character Kanji.default) Kanji.default
       , words = Dict.singleton (.word Word.default) Word.default
@@ -33,20 +36,26 @@ update msg model =
     ( model, Cmd.none )
 
 
-view : Model -> Html Msg
+view : Model -> Document Msg
 view model =
-    div []
-        (List.map
-            (\( char, kanji ) -> text <| String.fromChar kanji.character)
-            (Dict.toList model.kanji)
-        )
+    { title = "kanjibenkyo"
+    , body =
+        [ div []
+            (List.map
+                (\( char, kanji ) -> text <| String.fromChar kanji.character)
+                (Dict.toList model.kanji)
+            )
+        ]
+    }
 
 
-main : Program Never Model Msg
+main : Program () Model Msg
 main =
-    Html.program
+    Browser.application
         { view = view
         , init = init
         , update = update
         , subscriptions = always Sub.none
+        , onUrlChange = always NoOp
+        , onUrlRequest = always NoOp
         }
